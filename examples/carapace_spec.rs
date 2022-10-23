b334b3069e2c18ac8035ac9affb260421815b40f
rsteube <rsteube@users.noreply.github.com>
2022-10-23 19:42:01 +0200

    updated example

--- examples/carapace_spec.rs
+++ examples/carapace_spec.rs
@@ -4,29 +4,38 @@ use clap_complete::generate;
 use std::io;
 
 fn main() {
-    let mut cmd = Command::new("myapp")
+    let mut cmd = Command::new("example")
         .aliases(["alias1", "alias2"])
-        .about("some description")
+        .about("example command")
         .arg(
             Arg::new("help")
                 .long("help")
                 .short('h')
+                .help("show help")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("optional")
                 .long("optional")
+                .help("optional argument")
                 .require_equals(true)
                 .value_hint(ValueHint::Username),
         )
         .arg(
             Arg::new("value")
                 .short('v')
-                .help("takes value")
-                .value_hint(ValueHint::ExecutablePath),
+                .help("takes argument")
+                .value_parser(["one", "two", "three"]),
         )
-        .subcommand(Command::new("test").subcommand(Command::new("config")))
-        .subcommand(Command::new("hello"));
+        .subcommand(
+            Command::new("subcommand").about("example subcommand").arg(
+                Arg::new("command")
+                    .long("command")
+                    .short('c')
+                    .help("execute command")
+                    .value_hint(ValueHint::CommandName),
+            ),
+        );
 
     generate(Spec, &mut cmd, "myapp", &mut io::stdout());
 }
