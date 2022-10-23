050ae85b0010b4b9af2d25d978e9e70665162394
rsteube <rsteube@users.noreply.github.com>
2022-10-23 12:21:52 +0200

    flag signature

--- src/carapace_spec.rs
+++ src/carapace_spec.rs
@@ -1,5 +1,5 @@
 use clap::{
-    ArgAction, Command as ClapCommand,
+    Arg, ArgAction,
     ValueHint::{self, *},
 };
 use clap_complete::*;
@@ -7,33 +7,43 @@ use serde::Serialize;
 use std::collections::HashMap as Map;
 
 #[derive(Default, Serialize)]
-pub struct Command<'a> {
-    pub name: &'a str,
+pub struct Command {
+    pub name: String,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub aliases: Vec<&'a str>,
-    pub description: &'a str,
+    pub aliases: Vec<String>,
+    pub description: String,
     #[serde(skip_serializing_if = "Map::is_empty")]
-    pub flags: Map<&'a str, &'a str>,
+    pub flags: Map<String, String>,
     #[serde(skip_serializing_if = "Map::is_empty")]
-    pub persistentflags: Map<&'a str, &'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub completion: Option<Completion<'a>>,
+    pub persistentflags: Map<String, String>,
+    #[serde(skip_serializing_if = "Completion::is_empty")]
+    pub completion: Completion,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub commands: Vec<Command<'a>>,
+    pub commands: Vec<Command>,
 }
 
 #[derive(Default, Serialize)]
-pub struct Completion<'a> {
+pub struct Completion {
     #[serde(skip_serializing_if = "Map::is_empty")]
-    pub flag: Map<&'a str, Vec<&'a str>>,
+    pub flag: Map<String, Vec<String>>,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub positional: Vec<Vec<&'a str>>,
+    pub positional: Vec<Vec<String>>,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub positionalany: Vec<&'a str>,
+    pub positionalany: Vec<String>,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub dash: Vec<Vec<&'a str>>,
+    pub dash: Vec<Vec<String>>,
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub dashany: Vec<&'a str>,
+    pub dashany: Vec<String>,
+}
+
+impl Completion {
+    pub fn is_empty(&self) -> bool {
+        self.flag.is_empty()
+            && self.positionalany.is_empty()
+            && self.positionalany.is_empty()
+            && self.dash.is_empty()
+            && self.dashany.is_empty()
+    }
 }
 
 pub struct Spec;
@@ -43,46 +53,85 @@ impl Generator for Spec {
         format!("{}.yaml", name)
     }
 
-    fn generate(&self, cmd: &ClapCommand, _: &mut dyn std::io::Write) {
-        let mut command = Command {
-            name: cmd.get_name(),
-            aliases: cmd.get_all_aliases().collect(),
-            description: &cmd.get_about().unwrap_or_default().to_string(),
-            ..Default::default()
-        };
-
-        let _flags = Map::<&str, &str>::new();
-
-        for option in cmd.get_opts() {
-            let _short = option.get_short();
-            let long = option.get_long();
-            let name = long.unwrap_or("ARRR TODO");
-            let _description = option.get_help();
-            let action = action_for(option.get_value_hint());
-
-            if !action.is_empty() {
-                command
-                    .completion
-                    .get_or_insert_with(Default::default)
-                    .flag
-                    .insert(name, action);
-            }
-
-            let mut modifier = vec![];
-            if option.is_require_equals_set() {
-                modifier.insert(0, "?");
-            }
-            if let ArgAction::Set | ArgAction::Append | ArgAction::Count = option.get_action() {
-                modifier.insert(0, "*");
-            }
-        }
-
+    fn generate(&self, cmd: &clap::Command, _: &mut dyn std::io::Write) {
+        let command = command_for(cmd);
         let serialized = serde_yaml::to_string(&command).unwrap();
         println!("{}", serialized);
     }
 }
 
-fn action_for<'a>(hint: ValueHint) -> Vec<&'a str> {
+fn command_for(cmd: &clap::Command) -> Command {
+    Command {
+        name: cmd.get_name().to_owned(),
+        aliases: cmd.get_all_aliases().map(String::from).collect(),
+        description: cmd.get_about().unwrap_or_default().to_string(),
+        flags: flags_for(cmd),
+        completion: Completion {
+            flag: flag_completions_for(cmd),
+            ..Default::default()
+        },
+        commands: cmd.get_subcommands().map(command_for).collect(),
+        ..Default::default()
+    }
+}
+
+fn flags_for(cmd: &clap::Command) -> Map<String, String> {
+    let mut m = Map::new();
+    for option in cmd
+        .get_opts()
+        .filter(|o| !o.is_positional())
+        .map(|x| x.to_owned())
+        .chain(generator::utils::flags(cmd))
+        .collect::<Vec<Arg>>()
+    {
+        let signature = if let Some(long) = option.get_long() {
+            if let Some(short) = option.get_short() {
+                format!("-{} --{}", short, long)
+            } else {
+                format!("--{}", long)
+            }
+        } else {
+            format!("-{}", option.get_short().unwrap())
+        };
+        m.insert(
+            format!("{}{}", signature, modifier_for(&option)),
+            option.get_help().unwrap_or_default().to_string(),
+        );
+    }
+    m
+}
+
+fn flag_completions_for(cmd: &clap::Command) -> Map<String, Vec<String>> {
+    let mut m = Map::new();
+    for option in cmd.get_opts().filter(|o| !o.is_positional()) {
+        let name = option
+            .get_long()
+            .unwrap_or(&option.get_short().unwrap_or_default().to_string())
+            .to_owned();
+        m.insert(name, action_for(option.get_value_hint()));
+    }
+    m
+}
+
+fn modifier_for(option: &Arg) -> String {
+    let mut modifier = vec![];
+
+    if option.get_action().takes_values() {
+        if option.is_require_equals_set() {
+            modifier.push("?");
+        } else {
+            modifier.push("=");
+        }
+    }
+
+    if let ArgAction::Set | ArgAction::Append | ArgAction::Count = option.get_action() {
+        modifier.push("*");
+    }
+
+    modifier.join("")
+}
+
+fn action_for<'a>(hint: ValueHint) -> Vec<String> {
     match hint {
         // TODO actions for command are wrong
         Unknown => vec![],
@@ -100,4 +149,7 @@ fn action_for<'a>(hint: ValueHint) -> Vec<&'a str> {
         EmailAddress => vec![],
         _ => vec![],
     }
+    .into_iter()
+    .map(String::from)
+    .collect()
 }
